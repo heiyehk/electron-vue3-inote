40d14c4e769de2ac34fc1458bf8fd8f408ac8684
heiyehk <heiyehk@foxmail.com>
2021-08-26 16:47:25 +0800

    fix: 文件大小写问题

---
+++ src/components/Input.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="hy-input flex">
+    <div class="hy-input-box flex1">
+      <input :maxlength="maxlength" :readonly="readonly" v-model="inputValue" @input="changeInput" />
+    </div>
+    <div class="hy-number-control flex" v-if="control">
+      <div
+        class="hy-number-control-add hy-number-button"
+        :class="modelValue === max ? 'disabled' : ''"
+        @click="calculateDelay('add')"
+      >
+        <i class="iconfont icon-arrow-up"></i>
+      </div>
+      <div
+        class="hy-number-control-sub hy-number-button"
+        :class="modelValue === min ? 'disabled' : ''"
+        @click="calculateDelay('sub')"
+      >
+        <i class="iconfont icon-arrow-down"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  props: {
+    modelValue: [String, Number],
+    min: Number,
+    max: Number,
+    control: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    maxlength: String
+  },
+  emits: ['update:modelValue', 'on-change', 'on-input'],
+  setup(props, { emit }) {
+    let inputValue = ref();
+
+    if (props.type === 'number') {
+      inputValue.value = String(props.modelValue);
+    } else {
+      inputValue.value = props.modelValue || '';
+    }
+
+    const calculateDelay = (type: 'add' | 'sub') => {
+      const numberInput = Number(inputValue.value);
+      if (isNaN(numberInput)) return;
+      let afterNumber = 0;
+      switch (type) {
+        case 'add':
+          if (numberInput === 1000) return;
+          afterNumber = numberInput + 300;
+          break;
+        case 'sub':
+          if (numberInput === 100) return;
+          afterNumber = numberInput - 300;
+          break;
+      }
+      inputValue.value = String(afterNumber);
+      emit('update:modelValue', afterNumber);
+      emit('on-change', afterNumber);
+    };
+
+    const changeInput = () => {
+      emit('update:modelValue', inputValue.value);
+      emit('on-input', inputValue.value);
+    };
+
+    return {
+      inputValue,
+      calculateDelay,
+      changeInput
+    };
+  }
+});
+</script>
+
+<style lang="less" scoped>
+.hy-input {
+  border: 1px solid @border-color;
+  height: 24px;
+  border-radius: 4px;
+  overflow: hidden;
+  &-box {
+    input {
+      border-radius: 4px;
+      width: 100%;
+      height: 100%;
+      padding: 0 10px;
+      border: none;
+      box-sizing: border-box;
+      display: block;
+      cursor: auto;
+      background-color: transparent;
+    }
+  }
+  .hy-number-control {
+    flex-direction: column;
+    border-left: 1px solid @border-color;
+    .hy-number-button {
+      width: 30px;
+      position: relative;
+      height: 12px;
+      cursor: pointer;
+      &:hover {
+        background-color: @background-color;
+      }
+      &:first-child::before {
+        content: '';
+        width: 100%;
+        height: 1px;
+        background-color: @border-color;
+        bottom: -0.5px;
+        position: absolute;
+        z-index: 1;
+      }
+    }
+    .disabled {
+      cursor: no-drop;
+      background-color: @background-sub-color;
+      color: @text-sub-color;
+    }
+    .iconfont {
+      display: block;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+}
+</style>
---
+++ src/components/Switch.vue
@@ -0,0 +1,120 @@
+<template>
+  <div
+    class="hy-switch flex-items"
+    :class="[checkedStatus ? 'active' : '', disabled ? 'hy-switch-disabled' : '']"
+    role="switc"
+    :aria-checked="checkedStatus"
+    :style="styled"
+  >
+    <input v-model="checkedStatus" type="checkbox" true-value="true" false-value="false" />
+    <div class="hy-switch-core" @click="checkStatus">
+      <span class="hy-switch-action"></span>
+    </div>
+    <span class="hy-switch-checked-text">{{ checkedStatus ? '开' : '关' }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  props: {
+    modelValue: {
+      type: Boolean,
+      default: false
+    },
+    styled: String,
+    disabled: Boolean
+  },
+  emits: ['update:modelValue'],
+  setup(props, { emit }) {
+    const checkedStatus = ref(props.modelValue);
+
+    const checkStatus = () => {
+      if (props.disabled) return;
+      checkedStatus.value = !checkedStatus.value;
+      emit('update:modelValue', checkedStatus.value);
+    };
+
+    return {
+      checkedStatus,
+      checkStatus
+    };
+  }
+});
+</script>
+
+<style lang="less" scoped>
+@switchSize: 20px;
+@gap: 4px;
+
+.hy-switch {
+  position: relative;
+  height: @switchSize;
+  input {
+    position: absolute;
+    width: 0;
+    height: 0;
+    opacity: 0;
+    margin: 0;
+  }
+  &-core {
+    border-radius: 10px;
+    background-color: @gray-color !important;
+    width: 40px;
+    height: 100%;
+    cursor: pointer;
+    transition: background-color 0.4s;
+    &:active {
+      .hy-switch-action {
+        width: @switchSize;
+        transition: width 0.4s;
+      }
+    }
+  }
+  &-action {
+    width: @switchSize - @gap;
+    height: @switchSize - @gap;
+    background-color: #fff;
+    position: absolute;
+    border-radius: 100%;
+    top: 50%;
+    margin-top: -((@switchSize / 2) - (@gap / 2));
+    transform: translateX(@gap / 2);
+    transition: all 0.4s;
+  }
+  &-checked-text {
+    font-size: 12px;
+    margin-left: 4px;
+    color: @text-sub-color;
+  }
+}
+
+.active {
+  .hy-switch-core {
+    background-color: @success-color !important;
+    transition: background-color 0.4s;
+    &:active {
+      .hy-switch-action {
+        width: @switchSize;
+        transition: all 0.4s;
+        transform: translateX(@switchSize - 4px);
+      }
+    }
+  }
+  .hy-switch-action {
+    transition: all 0.4s;
+    transform: translateX(@switchSize);
+  }
+  .hy-switch-checked-text {
+    color: @success-color !important;
+  }
+}
+
+.hy-switch-disabled .hy-switch-core {
+  cursor: no-drop;
+  &:active .hy-switch-action {
+    width: 16px !important;
+  }
+}
+</style>
